6cd41813e2a65c64bd494a600eab238462d0f0a4
filipe-fff <pintovicentinifilipe@gmail.com>
2025-01-05 10:14:58 -0300

    criando o authService

---
+++ router service/aulas/2-router-guards/src/app/services/auth.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { jwtDecode } from "jwt-decode";
+import { map, Observable } from "rxjs";
+
+@Injectable({
+    providedIn: "root",
+})
+export class AuthService {
+    private readonly _httpClient = inject(HttpClient);
+
+    login(userName: string, password: string): Observable<{ token: string }> {
+       return this._httpClient.post<{ token: string }>("http://localhost:3000/login", { userName, password }).pipe(
+        map(resp => {
+            localStorage.setItem("access-token", resp.token);
+
+            return resp;
+        })
+       ); 
+    }
+
+    verifyToken(): Observable<{ valid: boolean, user: string }> {
+        const headers = new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("access-token"));
+
+        return this._httpClient.get<{ valid: boolean, user: string }>("http://localhost:3000/verify-token", { headers });
+    }
+
+    getUsersScopes(): string[] {
+        const token = localStorage.getItem("access-token");
+
+        if(!token) return [];
+
+        const decoded: any = jwtDecode(token);
+
+        return decoded.scopes;
+    }
+}
